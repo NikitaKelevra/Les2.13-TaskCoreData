b0fa5d6b7f41c3fbeba0079d030f2978f946ff56
Nikita Kelevra <nonamerz5@mail.ru>
2021-05-11 18:03:44 +0300

    Initial Commit

---
+++ Les2.13-TaskCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Les2.13-TaskCoreData
+//
+//  Created by Сперанский Никита on 11.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
