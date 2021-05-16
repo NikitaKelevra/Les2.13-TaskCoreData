ef488caac61e80f5f4f50c7c196b467fa9616d7c
Nikita Kelevra <nonamerz5@mail.ru>
2021-05-16 13:58:38 +0300

    full task with russian comment

--- Les2.13-TaskCoreData/Managers/StorageManager.swift
+++ Les2.13-TaskCoreData/Managers/StorageManager.swift
@@ -5,9 +5,75 @@
 //  Created by Сперанский Никита on 12.05.2021.
 //
 
-import Foundation
 import CoreData
 
-
+// StorageManager не должен хранить у себя какие массивы данных - данные хранятся в базе данных
+class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data stack
+    private var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Les2_13_TaskCoreData")   //название файла кор даты
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    
+    // MARK: - Сокращение
+    private let viewContext: NSManagedObjectContext  //сделали свойство, чтобы не писать "persistentContainer.viewContext"
+    
+    private init() {
+        viewContext = persistentContainer.viewContext // инициализатор нужен, чтобы инициализировать свойство!
+    }
+    
+    // MARK: - Public Methods
+//    должны быть реализованы все методы по управлению данными в базе - загрузка/удаление/редактирование/сохранение данных
+    
+    func fetchData(completion: (Result<[Task], Error>) -> Void) {   // type Result это перечисление(2 case), кот-е позволяет возвращать данные ассинхронно!
+        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()    // создаем запрос
+        
+        do {
+            let tasks = try viewContext.fetch(fetchRequest)     //формируем данные на основе запроса
+            completion(.success(tasks))     // помещаем массив двнных в comp-tion, при успехе вернет массив задач
+        } catch let error {
+            completion(.failure(error))
+        }
+    }
+    
+    func save(_ taskName: String, completion: (Task) -> Void) {
+        let task = Task(context: viewContext)   // создаем экземпляр модели
+        task.name = taskName
+        
+        completion(task)    // чтобы при вызове метода передать строку для отображения на экране
+        saveContext()
+    }
+    
+    func edit(_ task: Task, newName: String) {
+        task.name = newName
+        saveContext()
+    }
+    
+    func delete(_ task: Task) {
+        viewContext.delete(task)
+        saveContext()
+    }
+    
+    //MARK: - Core Data Saving support
+    func saveContext() {        //должен быть публичным так как вызывается в AppDeligate в методе ApplicationWillTerminate
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
 
 
--- Les2.13-TaskCoreData/ViewControllers/TaskListViewController.swift
+++ Les2.13-TaskCoreData/ViewControllers/TaskListViewController.swift
@@ -6,115 +6,162 @@
 //
 
 import UIKit
-import CoreData
-
-protocol TaskViewControllerDelegate {
-    func reloadData()
-}
 
 class TaskListViewController: UITableViewController {
     
-    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     private let cellID = "cell"
-    private var taskList: [Task] = []
+    private var tasks: [Task] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
-        setupNavigationBar()
-        fetchData()
-
+        setupView()
+        getData()
     }
-
-//      созданиеНавиБар и настройка внешнего вида
-    private func setupNavigationBar() {
-        title = "Task List"
-        navigationController?.navigationBar.prefersLargeTitles = true
-        let navBarAppearance = UINavigationBarAppearance()
-        navBarAppearance.configureWithOpaqueBackground()
-        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        navBarAppearance.backgroundColor = UIColor(
+    
+    private func setupView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+        view.backgroundColor = .white
+        setupNavigatorBar()
+    }
+    
+    private func setupNavigatorBar() {
+      
+        title = "Task List"   //заголовок экрана
+        navigationController?.navigationBar.prefersLargeTitles = true  //делаем заголовок большим шрифтом
+        
+// Navigation bar appearance
+        let navBarAppearance = UINavigationBarAppearance()  // внешний вид НавигейшнБар
+        navBarAppearance.configureWithOpaqueBackground()  // делает навиБар прозрачным
+        navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white] //цвет для обычного текста
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]  // цвет крупного текста
+        navBarAppearance.backgroundColor = UIColor(     // цвет фона голубоватый и слегка прозрачный
             red: 21/255,
             green: 101/255,
             blue: 192/255,
             alpha: 194/255)
         
+        // передача экземпляра - внешний вид НавиБара
         navigationController?.navigationBar.standardAppearance = navBarAppearance
+        // вид навиБара в момент скрола (листания) - параметры для каждого настраиваются отдельно
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
         
-        
-//        кнопка
+        //добавляем кнопку на экран
         navigationItem.rightBarButtonItem = UIBarButtonItem(
             barButtonSystemItem: .add,
-            target: self,                    //метод будет в самом VC
+            target: self,
             action: #selector(addNewTask)
         )
+        
+        //красим кнопку в белый цвет
         navigationController?.navigationBar.tintColor = .white
     }
-
-//    реализация действия кнопки
+    
     @objc private func addNewTask() {
-       showAlert(with: "New Task", and: "What do you want to do?")
+        showAlert()
     }
     
-    private func fetchData() {
-        let fetchRequest: NSFetchRequest<Task> = Task.fetchRequest()
-        do {
-            taskList = try context.fetch(fetchRequest)
-        } catch let error {
-            print(error)
+////      созданиеНавиБар и настройка внешнего вида
+//
+//
+//
+////        кнопка
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(
+//            barButtonSystemItem: .add,
+//            target: self,                    //метод будет в самом VC
+//            action: #selector(addNewTask)
+//        )
+//        navigationController?.navigationBar.tintColor = .white
+//    }
+
+
+    
+    
+    private func save(task: String) {
+        StorageManager.shared.save(task) { task in
+            self.tasks.append(task)
+            self.tableView.insertRows(
+                at: [IndexPath(row: self.tasks.count - 1,
+                               section: 0)],
+                with: .automatic
+            )
         }
-    }
-        
-    // MARK: - Alert Controller
-    private func showAlert(with title:String, and message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
-            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-            self.save(task)
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-        alert.addTextField { textField in
-            textField.placeholder = "New TasK"
-        }
-        present(alert, animated: true)
     }
     
-    private func save(_ taskName: String) {
-        guard let entityDiscription = NSEntityDescription.entity(forEntityName: "Task", in: context) else { return }
-        guard let task = NSManagedObject(entity: entityDiscription, insertInto: context) as? Task else { return }
-        task.title = taskName   // проверить не пустой ли - передавать только если содержаться данные
-        taskList.append(task)
-        
-        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
-        tableView.insertRows(at: [cellIndex], with: .automatic)
+//    Загрузка данных из базы и передача их в таблВью
+    private func getData() {
+        StorageManager.shared.fetchData { result in
+            switch result {
+            case .success(let tasks):  // если успех то вернет массив задач
+                self.tasks = tasks     // берем этот массив и передаем в свойства нашего класса
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
 // MARK: - UITableViewDataSource
 extension TaskListViewController {
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        taskList.count
-    }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
-        let task = taskList[indexPath.row]
-        var content  = cell.defaultContentConfiguration()
-        content.text = task.title
-        cell.contentConfiguration = content
-            return cell
+        let task = tasks[indexPath.row]
+        cell.textLabel?.text = task.name
+        
+        return cell
     }
 }
 
-// MARK: - TaskViewControllerDelegate
-extension TaskListViewController: TaskViewControllerDelegate {
-    func reloadData () {
-        fetchData()
-        tableView.reloadData()
+    // MARK: - UITableViewDelegate
+extension TaskListViewController {
+    
+    //Edit Task - didSelectRowAt вызывается когда мы тапаем по строке - затем вызываем АлертКонтр, в которой отображается название текущ задачи
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let task = tasks[indexPath.row]
+        showAlert(task: task) {
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    //Delete task
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let task = tasks[indexPath.row]
+        
+        if editingStyle == .delete {
+            tasks.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            StorageManager.shared.delete(task)
+        }
+    }
+}
+
+    // MARK: - Alert Controller
+extension TaskListViewController {
+    
+    private func showAlert(task: Task? = nil, completion: (() -> Void)? = nil) {    // comp-on для обновления интерфейса
+        
+        let title = task != nil ? "Update Task" : "New Task"
+        
+        let alert = AlertController(  // кастомный алерт с методом action, определяет как будем работать с данными
+            title: title,
+            message: "What do you want to do?",
+            preferredStyle: .alert)
+        
+        alert.action(task: task) { taskName in
+            if let task = task, let completion = completion {
+                StorageManager.shared.edit(task, newName: taskName)
+                completion()        //обновления интерфейса
+            } else {
+                self.save(task: taskName)
+            }
+        }
+        present(alert, animated: true)
     }
 }
